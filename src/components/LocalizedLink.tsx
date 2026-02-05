4b242023d569ccb68d336fcb67cef22c7f33de0b
Simex <1127870539@qq.com>
2026-02-05 23:16:16 +0800

    1

--- src/components/LocalizedLink.tsx
+++ src/components/LocalizedLink.tsx
@@ -18,11 +18,9 @@ interface LocalizedLinkProps extends Omit<React.ComponentPropsWithoutRef<typeof 
 export const LocalizedLink: React.FC<LocalizedLinkProps> = ({ href, children, ...props }) => {
   const { locale } = useI18n();
 
-  // 确保 href 是字符串
-  const hrefString = typeof href === 'string' ? href : href.pathname || '';
-
+  // href 已经是字符串类型，直接使用
   // 如果 href 已经是带有语言前缀的，则先移除，再添加当前语言的
-  const cleanHref = removeLocaleFromPath(hrefString);
+  const cleanHref = removeLocaleFromPath(href);
   const localizedHref = addLocaleToPath(cleanHref, locale);
 
   return (
