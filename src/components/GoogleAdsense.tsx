8c7ba3e4c699ea2ea33fb7ad867b4435e5b8be9d
Simex <1127870539@qq.com>
2026-02-12 16:46:00 +0800

    1

--- src/components/GoogleAdsense.tsx
+++ src/components/GoogleAdsense.tsx
@@ -7,7 +7,7 @@ import Script from 'next/script';
  *
  * 官方要求将以下代码放在每个页面的 <head> 中：
  *
- * <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2256987655997953"
+ * <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2256987655979539"
  *      crossorigin="anonymous"></script>
  *
  * 这里通过 next/script 在 App Router 下统一注入，Next.js 会自动把脚本插入到 <head>。
@@ -22,9 +22,9 @@ export function GoogleAdsense() {
     <Script
       id="google-adsense"
       async
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2256987655997953"
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2256987655979539"
       crossOrigin="anonymous"
-      strategy="beforeInteractive" // 尽量贴近官方要求：在 <head> 中尽早加载
+      strategy="beforeInteractive"
     />
   );
 }
